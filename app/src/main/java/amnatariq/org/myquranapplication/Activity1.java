783574cc95ddd5fa26d88d7138b665bf9523fbe9
Amna <aamnaht4@gmail.com>
2023-01-28 19:03:06 +0500

    adding onclick listener -- onclick function for each cardview

--- app/src/main/java/amnatariq/org/myquranapplication/Activity1.java
+++ app/src/main/java/amnatariq/org/myquranapplication/Activity1.java
@@ -7,7 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class Activity1 extends AppCompatActivity {
+public class Activity1 extends AppCompatActivity implements View.OnClickListener {
 
     private CardView D1,D2,D3,D4;
 
@@ -37,6 +37,9 @@ public class Activity1 extends AppCompatActivity {
         Intent i;
         switch (v.getId()){
             case R.id.d1 : i = new Intent(this,d1.class); startActivity(i);break;
+            case R.id.d2 : i = new Intent(this,d2.class); startActivity(i);break;
+            case R.id.d3 : i = new Intent(this,d3.class); startActivity(i);break;
+            case R.id.d4 : i = new Intent(this,d4.class); startActivity(i);break;
         }
     }
 }
