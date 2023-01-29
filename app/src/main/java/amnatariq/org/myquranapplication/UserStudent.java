c912c186cdd51d104698af1c8b47370920017587
Amna <aamnaht4@gmail.com>
2023-01-29 23:10:04 +0500

    adding new data to student class

--- app/src/main/java/amnatariq/org/myquranapplication/UserStudent.java
+++ app/src/main/java/amnatariq/org/myquranapplication/UserStudent.java
@@ -9,13 +9,27 @@ public class UserStudent {
     private int sabki;
     private int manzil;
 
-    public UserStudent(String name, String information, String status, int sabak, int sabki, int manzil) {
+    private boolean sabakStatus;
+    private boolean sabkiStatus;
+    private boolean manzilStatus;
+
+    private String sabakIncorrect;
+    private String sabkiIncorrect;
+    private String manzilIncorrect;
+
+    public UserStudent(String name, String information, String status, int sabak, int sabki, int manzil, boolean sabakStatus, boolean sabkiStatus, boolean manzilStatus, String sabakIncorrect, String sabkiIncorrect, String manzilIncorrect) {
         this.name = name;
         this.information = information;
         this.status = status;
         this.sabak = sabak;
         this.sabki = sabki;
         this.manzil = manzil;
+        this.sabakStatus = sabakStatus;
+        this.sabkiStatus = sabkiStatus;
+        this.manzilStatus = manzilStatus;
+        this.sabakIncorrect = sabakIncorrect;
+        this.sabkiIncorrect = sabkiIncorrect;
+        this.manzilIncorrect = manzilIncorrect;
     }
 
     public String getName() {
@@ -64,6 +78,54 @@ public class UserStudent {
 
     public void setManzil(int manzil) {
         this.manzil = manzil;
+    }
+
+    public boolean isSabakStatus() {
+        return sabakStatus;
+    }
+
+    public void setSabakStatus(boolean sabakStatus) {
+        this.sabakStatus = sabakStatus;
+    }
+
+    public boolean isSabkiStatus() {
+        return sabkiStatus;
+    }
+
+    public void setSabkiStatus(boolean sabkiStatus) {
+        this.sabkiStatus = sabkiStatus;
+    }
+
+    public boolean isManzilStatus() {
+        return manzilStatus;
+    }
+
+    public void setManzilStatus(boolean manzilStatus) {
+        this.manzilStatus = manzilStatus;
+    }
+
+    public String getSabakIncorrect() {
+        return sabakIncorrect;
+    }
+
+    public void setSabakIncorrect(String sabakIncorrect) {
+        this.sabakIncorrect = sabakIncorrect;
+    }
+
+    public String getSabkiIncorrect() {
+        return sabkiIncorrect;
+    }
+
+    public void setSabkiIncorrect(String sabkiIncorrect) {
+        this.sabkiIncorrect = sabkiIncorrect;
+    }
+
+    public String getManzilIncorrect() {
+        return manzilIncorrect;
+    }
+
+    public void setManzilIncorrect(String manzilIncorrect) {
+        this.manzilIncorrect = manzilIncorrect;
     }
 }
 
