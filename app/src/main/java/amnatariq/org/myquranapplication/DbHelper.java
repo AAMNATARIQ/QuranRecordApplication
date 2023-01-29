e6bb72c7e5431e18fc69cbda094075e789e9be2a
Amna <aamnaht4@gmail.com>
2023-01-29 23:41:55 +0500

    adding buttons to user information

--- app/src/main/java/amnatariq/org/myquranapplication/DbHelper.java
+++ app/src/main/java/amnatariq/org/myquranapplication/DbHelper.java
@@ -7,9 +7,27 @@ import android.database.sqlite.SQLiteOpenHelper;
 import androidx.annotation.Nullable;
 
 public class DbHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME ="MyDB.db";
+    private static final int DATABASE_VERSION =1;
+
+    private SQLiteDatabase db;
+
     @Override
     public void onCreate(SQLiteDatabase db) {
         //String createTableStatement = "CREATE TABLE USERSTUDENT_TABLE( StudentId Integer PRIMARY KEY AUTOINCREMENT,"
+        this.db =db;
+//        final String SQL_CREATE_QUESTIONS_TABLE = "CREATE TABLE "+
+//                UserStudent_TABLE + " ( " +
+//                QuestionsTable._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "+
+//                QuestionsTable.COLUMN_QUESTION + " TEXT, " +
+//                QuestionsTable.COLUMN_OPTION1 + " TEXT, " +
+//                QuestionsTable.COLUMN_OPTION2 + " TEXT, " +
+//                QuestionsTable.COLUMN_OPTION3 + " TEXT, " +
+//                QuestionsTable.COLUMN_ANSWER + " TEXT, " +
+//                QuestionsTable.COLUMN_MYANSWER + " TEXT" + ")";
+//
+//        db.execSQL(SQL_CREATE_QUESTIONS_TABLE);
+//        fillQuestionsTable();
     }
 
     @Override
