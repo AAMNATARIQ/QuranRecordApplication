aafb2cdfbb1e67bd67156fc136f40e238051c990
Amna <aamnaht4@gmail.com>
2023-01-30 22:36:48 +0500

    adding activities for all the sabak sabki and manzil details

---
+++ app/src/main/java/amnatariq/org/myquranapplication/Sabaki.java
@@ -0,0 +1,14 @@
+package amnatariq.org.myquranapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Sabaki extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sabaki);
+    }
+}
--- app/src/main/java/amnatariq/org/myquranapplication/UserInformation.java
+++ app/src/main/java/amnatariq/org/myquranapplication/UserInformation.java
@@ -2,24 +2,57 @@ package amnatariq.org.myquranapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
-public class UserInformation extends AppCompatActivity implements View.OnClickListener {
+public class UserInformation extends AppCompatActivity{
 
-
+    Button btnv1,btnv2,btnv3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_information);
 
+        btnv1 = (Button)findViewById(R.id.btnviewone);
+        btnv2 = (Button)findViewById(R.id.btnviewtwo);
+        btnv3 = (Button)findViewById(R.id.btnviewthree);
 
+        btnv1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateSabakView();
+            }
+        });
+        btnv2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateSabakiView();
+            }
+        });
+        btnv3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateManzilView();
+            }
+        });
 
     }
-
-    @Override
-    public void onClick(View v) {
-
+    public void updateSabakView()
+    {
+        Intent intent = new Intent(this,Sabak.class);
+        startActivity(intent);
+    }
+    public void updateSabakiView()
+    {
+        Intent intent = new Intent(this,Sabaki.class);
+        startActivity(intent);
+    }
+    public void updateManzilView()
+    {
+        Intent intent = new Intent(this,Manzil.class);
+        startActivity(intent);
     }
 }
